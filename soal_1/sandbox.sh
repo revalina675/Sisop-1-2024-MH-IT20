3adfa5364c023b7782c4b5ffba996c0e5db5054c
revalina675 <150936800+revalina675@users.noreply.github.com>
2024-03-25 22:11:19 +0700

    Update sandbox.sh

--- soal_1/sandbox.sh
+++ soal_1/sandbox.sh
@@ -1,3 +1,3 @@
 echo -n "Customer's Name: "
 read name
-awk -v nama="$name" -F ',' '$6==nama{print "Order Date: "$2 ", Quantity: "$18}' Sandbox.csv 
+  awk -v nama="$name" -F ',' '$6==nama{print "Order Date: "$2 ", Quantity: "$18}' Sandbox.csv 
